8e06e009649d4acccd32a5aa5b84cd72247cd0e5
Mukesh <lakkakulababblu@gmail.com>
2025-07-05 12:33:12 +0530

    solution3

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const headlines = [
